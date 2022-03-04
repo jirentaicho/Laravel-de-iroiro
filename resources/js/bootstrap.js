2b34b691dd77b419d038cb847097b5e2e77765ed
jiren <yujienjoyfever@yahoo.co.jp>
2022-03-04 14:32:37 +0900

    token header

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,7 +9,9 @@ window._ = require('lodash');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
+// add Token Info
+window.axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('access_token')}`;
+window.axios.defaults.headers.common['Content-Type'] = "application/json";
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
